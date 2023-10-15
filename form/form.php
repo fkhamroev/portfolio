2f7b0ece747155aed1165cd83751c5e343206776
Firuz Khamroev <fkhamroev@icloud.com>
2023-10-15 09:11:37 -0700

    form3

--- form/form.php
+++ form/form.php
@@ -3,22 +3,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    
+
     // Укажите адрес, на который нужно отправить сообщение
     $to = "fkhamroev@icloud.com";
-    
+
     // Заголовки письма
     $subject = "Сообщение с формы на вашем сайте";
     $headers = "From: $email";
-    
+
     // Отправка письма
     $sent = mail($to, $subject, $message, $headers);
-    
+
     if ($sent) {
-        echo "succes";
-    }
-    else {
+        header('../pages/thanks.html');
+    } else {
         echo "Error";
     }
 }
-
