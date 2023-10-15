4224ecf1a57f52a46bb94aebb4bb77639cc02b14
Firuz Khamroev <fkhamroev@icloud.com>
2023-10-15 09:16:11 -0700

    form3

--- js/script.js
+++ js/script.js
@@ -24,3 +24,34 @@ var swiper = new Swiper(".mySwiper", {
 //   e.preventDefault();
 //   redirectToPage();
 // });
+
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Предотвращаем стандартное действие отправки формы
+
+  // Собираем данные из формы
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Создаем объект FormData для отправки данных на сервер
+  var formData = new FormData();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("message", message);
+
+  // Отправляем данные на серверный скрипт (замените "form.php" на ваш серверный скрипт)
+  fetch("form.php", {
+    method: "POST",
+    body: formData,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        alert("Ваше сообщение успешно отправлено.");
+      } else {
+        alert("Произошла ошибка при отправке сообщения.");
+      }
+    })
+    .catch(function (error) {
+      console.error("Ошибка при отправке данных на сервер:", error);
+    });
+});
