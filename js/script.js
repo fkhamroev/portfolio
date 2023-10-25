43c9a91a4791cea21ad392c06e99ecace335e2b7
Firuz <fkhamroev@icloud.com>
2023-10-25 19:11:03 +0500

    later

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
@@ -8,50 +8,25 @@ var swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 3000,
   },
+  breakpoints: {
+    // Когда ширина экрана меньше или равна 992px
+    992: {
+      slidesPerView: 2,
+    },
+  },
 });
 
-// function scrollToTop() {
-//   window.scrollTo(0, 0);
-// }
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
 
-// function redirectToPage(url) {
-//   window.location.href = "../pages/thanks.html";
-// }
+function redirectToPage(url) {
+  window.location.href = "../pages/thanks.html";
+}
 
-// let submit = document.querySelector(".form-submit");
+let submit = document.querySelector(".form-submit");
 
-// submit.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   redirectToPage();
-// });
-
-document.getElementById("myForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // Предотвращаем стандартное действие отправки формы
-
-  // Собираем данные из формы
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
-
-  // Создаем объект FormData для отправки данных на сервер
-  let formData = new FormData();
-  formData.append("name", name);
-  formData.append("email", email);
-  formData.append("message", message);
-
-  // Отправляем данные на серверный скрипт (замените "form.php" на ваш серверный скрипт)
-  fetch("form.php", {
-    method: "POST",
-    body: formData,
-  })
-    .then(function (response) {
-      if (response.ok) {
-        window.location("../pages/thanks.html");
-      } else {
-        alert("Произошла ошибка при отправке сообщения.");
-      }
-    })
-    .catch(function (error) {
-      console.error("Ошибка при отправке данных на сервер:", error);
-    });
+submit.addEventListener("submit", function (e) {
+  e.preventDefault();
+  redirectToPage();
 });
